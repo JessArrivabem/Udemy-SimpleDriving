2b6a43d0488980083c738fba1cc4a4390ae339c4
Jessica Arrivabem <jessica.arrivabem@gmail.com>
2024-12-02 11:27:00 -0300

    Steering Car added

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,12 +6,27 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSecond = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int steerValue;
+
+    void Start()
+    {
+        Debug.Log($"w {Screen.width}, h {Screen.height}");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        speed += speedGainPerSecond * Time.deltaTime;
+        speed += speedGainPerSecond * Time.deltaTime; // increases the speed
 
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); // moves object on Z axis
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
