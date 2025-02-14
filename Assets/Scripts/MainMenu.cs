e93c4b94a37e5431a664f1e3a6119935f1e16f82
Jessica Arrivabem <jessica.arrivabem@gmail.com>
2025-02-14 10:34:39 -0300

    Android Notification

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,15 +9,61 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
         highScoreText.text = $"High Score: {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty); // checks if energy should be regenerated
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy); // saves the new value
+            }
+
+        }
+
+        energyText.text = $"Play ({energy})";
     }
     public void Play()
     {
+        if (energy < 1) { return; } // can't play
+
+        energy--;
+
+        PlayerPrefs.SetInt(EnergyKey, energy); // saves the new value
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+
+#if UNITY_ANDROID
+            androidNotificationHandler.ScheduleNotification(energyReady);
+#endif
+        }
+
         SceneManager.LoadScene(1);
     }
 }
